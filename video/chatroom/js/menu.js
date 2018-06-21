b3787208197899abcc90eac0d0f27499be025d92
frankguoo <frank61012@gmail.com>
2018-06-21 15:07:03 +0800

    2
    2

---
+++ video/chatroom/js/menu.js
@@ -0,0 +1,95 @@
+// JavaScript Document
+
+			var menu = document.getElementById('menu');
+          
+			menu.innerHTML =
+				'<div id="header">&nbsp;</div>\n'+
+				'<div class="container">\n'+
+				'<div class="row">\n'+
+				'<div class="col-lg-12">\n'+
+				'<div class="site_nav">\n'+
+				'<a href="http://shopping.pchome.com.tw" target="_blank"><img src="http://www.pchomechatroom.com/pchometv/video/img/logo.png" width="165" alt="" /></a>\n'+
+				'<ul class="main_site">\n'+
+				'<li><a href="http://shopping.pchome.com.tw" target="_blank">線上購物</a>&nbsp;|</li>\n'+
+				'<li><a href="http://24h.pchome.com.tw" target="_blank">24h購物</a>&nbsp;|</li>\n'+
+				'<li><a href="http://mall.pchome.com.tw" target="_blank">購物中心</a>&nbsp;|</li>\n'+
+				'<li><a href="http://24h.pchome.com.tw/books" target="_blank">書店</a></li>\n'+
+				'</ul>\n'+
+				'<ul class="sub_site">\n'+
+				'<li><a href="https://ecvip.pchome.com.tw/" target="_blank">顧客中心</a>&nbsp;|</li>\n'+
+				'<li><a href="https://ecvip.pchome.com.tw/web/service/vendor/cooperation" target="_blank">廠商合作</a>&nbsp;|</li>\n'+
+				'<li><a href="https://ecvip.pchome.com.tw/?0x160330d458f1224661d4a97b3cb2369393f1b9860843c13c96446f2cd38fd32725ffaaeb00c5b2e217ae83db147ce5d5&url=http%3A%2F%2Fmall.pchome.com.tw%2F" target="_blank">故障通報</a>&nbsp;|</li>\n'+
+				'<li><a href="http://www.pchome.com.tw/" target="_blank">PChome</a></li>\n'+
+				'</ul>\n'+
+				'</div>\n'+
+				'</div>\n'+
+				'</div>\n'+
+				'</div>\n'+
+				
+				'<nav class="navbar navbar-default navbar-org navbar-fixed-top" role="navigation">\n'+
+				'<div id="intro">\n' +
+				'<div class="container">\n' +
+				'<div class="row">\n' +
+				'<div class="col-lg-12"> \n' +
+				'<h1><a href="http://www.pchomechatroom.com/pchometv/video/index.html" style="color:#97cad2;"><img src="http://www.pchomechatroom.com/pchometv/video/img/video-logo.png" width="220"  alt=""/></a></h1>\n' +
+				'</div>\n' +
+				'</div>\n' +
+				'</div>\n' +
+				'</div>\n' +
+				
+				'<div class="container">\n' +
+				'<div class="navbar-header page-scroll">\n' +
+                '<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">\n' +
+                '<span class="sr-only">Toggle navigation</span>\n' +
+                '<span class="icon-bar"></span>\n' +
+                '<span class="icon-bar"></span>\n' +
+                '<span class="icon-bar"></span>\n' +
+                '</button>\n' +
+                '<a class="navbar-brand page-scroll" href="http://www.pchomechatroom.com/pchometv/video/index.html" >首頁</a>\n' +
+            	'</div>\n' +
+				
+				'<div class="collapse navbar-collapse navbar-ex1-collapse">\n'+
+				'<ul class="nav navbar-nav">\n' +
+				'<li class="hidden">\n' +
+				'<a class="page-scroll" href="#"></a>\n' +
+				'</li>\n' +
+
+				'<li class="dropdown">\n' +
+				'<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" style="background-color:#0081AD;">名人360</a>\n' +
+				'<ul class="dropdown-menu">\n' +
+				'<li><a tabindex="-1" href="http://www.pchomechatroom.com/pchometv/video/chatroom/180606/index.html">最新節目</a></li>\n' +
+				'<li><a href="http://www.pchomechatroom.com/pchometv/video/chatroom/history.html" tabindex="-1">精彩回顧</a></li>\n' +
+				'<li><a href="http://www.pchomechatroom.com/pchometv/video/chatroom/rules.html" tabindex="-1">抽獎活動</a></li>\n' +
+				'</ul>\n' +
+				'</li>\n' +
+			
+				'<li class="dropdown">\n' +
+				'<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">P編開賣啦</a>\n' +
+				'<ul class="dropdown-menu">\n' +
+				'<li><a tabindex="-1" href="http://www.pchomechatroom.com/pchometv/video/hotsale/180524/index.html">最新節目</a></li>\n' +
+				'<li><a href="http://www.pchomechatroom.com/pchometv/video/hotsale/history.html" tabindex="-1">精彩回顧</a></li>\n' +
+				'</ul>\n' +
+				'</li>\n' +
+
+				'<li class="dropdown">\n' +
+				'<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" >美味生活選好物</a>\n'+
+				'<ul class="dropdown-menu">\n' +
+                '<li><a tabindex="-1" href="http://www.pchomechatroom.com/pchometv/video/how-living/180110/index.html">最新節目</a></li>\n' +
+				'<li><a href="http://www.pchomechatroom.com/pchometv/video/how-living/history.html" tabindex="-1">精彩回顧</a></li>\n' +
+				'</ul>\n' +
+                '</li>\n'+
+				'</ul>\n'+
+                
+				'</div>\n'+
+				'</div>\n'+
+				'</nav>\n';
+
+
+				/*<div id="lightbg" class="lightbox"><a href="#program" style="color:#FAFAFA">節目表</a><span class="news">ＮEW</span></div>
+                <div class="lightbox">
+					<ul id="marquee0" class="marquee">
+						<li><span class="timers">20:00&nbsp;</span>&nbsp;&nbsp;&nbsp;&nbsp;<span class="things">雨揚居士&nbsp;&nbsp;即將播出</span></li>
+						<li><span class="timers">20:00&nbsp;</span>&nbsp;&nbsp;&nbsp;&nbsp;<span class="things">雨揚居士&nbsp;&nbsp;即將播出</span></li>
+						<li><span class="timers">20:00&nbsp;</span>&nbsp;&nbsp;&nbsp;&nbsp;<span class="things">雨揚居士&nbsp;&nbsp;即將播出</span></li>
+					</ul>
+          		</div>*/
