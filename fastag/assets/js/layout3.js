3ad024be17ea5f08e05c84d16923e5110fcbe46e
frankguoo <frank61012@gmail.com>
2018-07-04 11:50:33 +0800

    4
    4

---
+++ fastag/assets/js/layout3.js
@@ -0,0 +1,20 @@
+
+
+$( function(){
+
+
+	// 品牌輪播
+	$('.store_discount .owl-carousel').owlCarousel({
+	    loop: false,
+	    margin: 10,
+	    autoplay: true,
+	    autoplayTimeout: 5000,
+	    nav: true,
+	    navText : ["<",">"],
+	    items: 1
+	});
+
+});
+
+
+
