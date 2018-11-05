cc0d0001311ee6127f484e01f0cab97bac7fb65a
frankguoo <frank61012@gmail.com>
2018-11-05 16:45:11 +0800

    MMM
    MMM

---
+++ LineageM/js/menu.js
@@ -0,0 +1,145 @@
+// JavaScript Document
+	var menu = document.getElementById('mainmenu');
+    menu.innerHTML =
+		
+		/*手機版版頭 開始*/		
+        '<div class="header_mobile">\n'+
+            '<ul class="top">\n'+
+                '<li class="logo">\n'+
+					'<h2 style="margin-top: 0px;margin-bottom: 0px;">\n'+
+						'<a href="https://www.pchomefastag.com.tw?utm_source=officialwebsite&utm_medium=hashtag_ft&utm_campaign=ft">\n'+
+							'<img src="assets/img/logo_ft_mobile_1.svg" alt="智慧標籤fastag">\n'+
+						'</a>\n'+
+					'</h2>\n'+
+				'</li>\n'+
+				
+                '<li class="coinP">\n'+
+                     '<a class="download_app" href="http://24h.m.pchome.com.tw/appget.php" target="_blank">\n'+
+                        '<img class="icon_p" src="assets/img/logo_app_2.svg" alt="pchome24h購物">\n'+
+                           '<span class="icon_word">App下載</span>\n'+
+					 '</a>\n'+
+				'</li>\n'+
+				
+                '<li class="card">\n'+
+					'<a class="download_app" href="https://24h.m.pchome.com.tw/NFC/scan?utm_source=officialwebsite&utm_medium=btn_side&utm_campaign=ft" target="_blank">\n'+ 
+						'<img class="icon_p" src="assets/img/logo_sensor-03.svg" alt="智慧標籤感應">\n'+
+					'</a>\n'+
+				'</li>\n'+
+            '</ul>\n'+
+        '</div>\n'+
+		/*手機版版頭 結束*/		
+				
+        /*手機版選單 開始*/
+        '<div class="nav_mobile navbar-static-top">\n'+
+            '<nav class="navbar">\n'+
+                '<ul class="nav">\n'+
+                    '<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="javascript:;" role="button" aria-haspopup="true" aria-expanded="false">智慧標籤&nbsp;<span class="glyphicon glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a>\n'+
+                        '<div class="dropdown-menu">\n'+
+                            '<ul class="nav nav-tabs nnn" role="tablist">\n'+
+                                '<li><a href="https://www.pchomefastag.com.tw/sites/fastag#iwantgetfastag＿block" id="iwantgetfastagm">智慧標籤介紹</a></li>\n'+
+                                '<li><a href="https://www.pchomefastag.com.tw/sites/fastag#feature＿block" id="featurem">感應3步驟</a></li>\n'+
+                                '<li><a href="https://www.pchomefastag.com.tw/sites/fastag#getfastag＿block" id="getfastagm">如何獲得</a></li><br>\n'+
+                                '<li><a href="https://www.pchomefastag.com.tw/sites/fastag#selectedbrand＿block" id="selectedbrandm">精選品牌</a></li>\n'+
+                            '</ul>\n'+
+                        '</div>\n'+
+                    '</li>\n'+
+
+                    '<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="javascript:;" role="button" aria-haspopup="true" aria-expanded="false">品牌優惠&nbsp;<span class="glyphicon glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a>\n'+
+                        '<div class="dropdown-menu">\n'+
+                            '<ul class="nav nav-tabs" role="tablist">\n'+
+                                '<li><a href="//www.pchomefastag.com.tw/sites/fastag/index_05.html">品牌優惠列表</a></li>\n'+
+                            '</ul>\n'+
+                        '</div>\n'+
+                  
+                    '<li class="nav-item dropdown"><a class="nav-link dropdown-toggle" data-toggle="dropdown" href="javascript:;" role="button" aria-haspopup="true" aria-expanded="false">常見問題&nbsp;<span class="glyphicon glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a>\n'+
+						'<div class="dropdown-menu">\n'+
+							'<ul class="nav nav-tabs nnn" role="tablist">\n'+
+								'<li>\n'+
+									'<a href="//www.pchomefastag.com.tw/sites/fastag/index_04.html">問與答</a>\n'+
+								'</li>\n'+
+
+								'<li>\n'+
+									'<a href="https://ecvip.pchome.com.tw/web/pages/contactinfo.htm?utm_source=officialwebsite&utm_medium=contact&utm_campaign=ft" target="_blank">聯絡客服</a>\n'+
+								'</li>\n'+
+
+								'<li>\n'+
+									'<a href="https://ecvip.pchome.com.tw/web/service/vendor/cooperation?utm_source=officialwebsite&utm_medium=cooperation&utm_campaign=ft" target="_blank">合作洽談</a>\n'+
+								'</li>\n'+	
+							'</ul>\n'+
+						'</div>\n'+	
+					'</li>\n'+                   
+                '</ul>\n'+
+            '</nav>\n'+
+        '</div>\n'+
+        /*手機版選單 結束*/
+
+
+        /*桌機版版頭+選單 開始*/
+        '<div class="navbar navbar-collapse navbar-static-top header_desktop">\n'+
+            '<div class="container">\n'+
+                '<ul class="nav navbar-nav">\n'+
+                    '<li class="logo"><h1><a href="https://www.pchomefastag.com.tw?utm_source=officialwebsite&utm_medium=hashtag_ft&utm_campaign=ft"><img src="assets/img/logo_ftdd.svg" alt="智慧標籤fastag"></a></h1></li>\n'+
+
+                    '<li class="text only dropdown">\n'+
+                        '<div class="fixbg" style="position: relative;"></div><a herf=""class="dropdown-toggle" href="javascript:;" data-toggle="dropdown" aria-expanded="false">智慧標籤<span class="glyphicon glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a>\n'+
+                        '<ul class="dropdown-menu" role="menu" style="display: none;">\n'+
+                            
+                             '<li>\n'+
+                                '<a href="https://www.pchomefastag.com.tw/sites/fastag#iwantgetfastag＿block" id="iwantgetfastag">\n'+
+                                    '<div class="btn">智慧標籤介紹</div>\n'+
+                                '</a>\n'+
+                            '</li>\n'+
+                            '<li>\n'+
+                                '<a href="https://www.pchomefastag.com.tw/sites/fastag#feature＿block" id="feature">\n'+
+                                    '<div class="btn">感應3步驟</div>\n'+
+                                '</a>\n'+
+                            '</li>\n'+
+                           
+                           '<li>\n'+
+                                '<a href="https://www.pchomefastag.com.tw/sites/fastag#getfastag＿block" id="getfastag">\n'+
+                                    '<div class="btn">如何獲得</div>\n'+
+                                '</a>\n'+
+                            '</li>\n'+
+                            '<li>\n'+
+                                '<a href="https://www.pchomefastag.com.tw/sites/fastag#selectedbrand＿block" id="selectedbrand">\n'+
+                                    '<div class="btn">精選品牌</div>\n'+
+                                '</a>\n'+
+                            '</li>\n'+
+                        '</ul>\n'+
+                    '</li>\n'+
+                    '<li class="text only dropdown">\n'+
+                        '<div class="fixbg" style="position: relative;"></div><a class="dropdown-toggle" href="javascript:;" data-toggle="dropdown">品牌優惠<span class="glyphicon glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a>\n'+
+                        '<ul class="dropdown-menu" role="menu">\n'+
+                            '<li>\n'+
+                                '<a href="//www.pchomefastag.com.tw/sites/fastag/index_05.html">\n'+
+                                    '<div class="btn">品牌優惠列表</div>\n'+
+                                '</a>\n'+
+                            '</li>\n'+
+                        '</ul>\n'+
+                    '</li>\n'+
+                    '<li class="text only dropdown">\n'+
+						'<div class="fixbg" style="position: relative;"></div><a class="dropdown-toggle" href="javascript:;" data-toggle="dropdown">常見問題<span class="glyphicon glyphicon glyphicon-chevron-down" aria-hidden="true"></span></a>\n'+
+                        '<ul class="dropdown-menu" role="menu">\n'+
+							'<li>\n'+
+								'<a href="//www.pchomefastag.com.tw/sites/fastag/index_04.html"><div class="btn">問與答</div></a>\n'+
+							'</li>\n'+
+							
+							'<li>\n'+
+								'<a href="https://ecvip.pchome.com.tw/web/pages/contactinfo.htm?utm_source=officialwebsite&utm_medium=contact&utm_campaign=ft" target="_blank"><div class="btn">聯絡客服</div></a>\n'+
+							'</li>\n'+
+							
+							'<li>\n'+
+								'<a href="https://ecvip.pchome.com.tw/web/service/vendor/cooperation?utm_source=officialwebsite&utm_medium=cooperation&utm_campaign=ft" target="_blank"><div class="btn">合作洽談</div></a>\n'+
+							'</li>\n'+	
+						'</ul>\n'+
+					'</li>\n'+
+                    '<li class="coinP">\n'+
+                        '<a class="download_app" href="http://24h.m.pchome.com.tw/appget.php" target="_blank">\n'+
+                           '<img class="icon_p" src="assets/img/logo_app_2.svg" alt="pchome24h購物">\n'+
+                             '<span class="icon_word">App下載</span>\n'+
+						'</a>\n'+
+                    '</li>\n'+
+                '</ul>\n'+
+            '</div>\n'+
+        '</div>\n';
+        /*桌機版版頭+選單 結束*/
