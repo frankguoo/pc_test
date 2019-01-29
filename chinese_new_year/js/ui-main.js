296dec1a993bdaa879f6f00601f58017f637d255
PChomeFrank <frank61012@gmail.com>
2019-01-29 12:13:03 +0800

    10
    10

--- chinese_new_year/js/ui-main.js
+++ chinese_new_year/js/ui-main.js
@@ -116,5 +116,5 @@ $(function()
 			
         }) ;
 
-    $("#BOX_A img").replace('ec1img.pchome.com.tw', "b.ecimg.tw")    
+    $("#BOX_A img").replace('ec1img.pchome.com.tw', "b.ecimg.tw");   
 });
