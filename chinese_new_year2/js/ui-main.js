3673394d31fd1dde68f22b2957e518c4e34ab32f
PChomeFrank <frank61012@gmail.com>
2019-01-29 15:13:10 +0800

    3
    3

--- chinese_new_year2/js/ui-main.js
+++ chinese_new_year2/js/ui-main.js
@@ -25,17 +25,10 @@ $(function()
                             .append('<li id="box'+idx+'" class="box">'+
                                         '<a href="'+_node.Link.Url+'" target="_blank"><div class="imgs"><img class="ggg" src="'+_node.Img2.Src+'" alt=""></div></a>'+
                                      '</li>');	 
+                        $("img").attr('src',_node.Img2.Src.replace('ec1img.pchome.com.tw','b.ecimg.tw'));	             
                                                                                    
                     });
-
-                    $.each(_data.Nodes, function(idx, _node)
-                    {
-						
-						$(".ggg").attr('src',_node.Img2.Src.replace('ec1img.pchome.com.tw','b.ecimg.tw'));	 
-                                                                                   
-                    });
-                    
-					
+						 
                 });
             });
 			
