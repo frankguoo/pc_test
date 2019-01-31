889f3e3fa299c57400ba094a217ecf953f0b8fd1
PChomeFrank <frank61012@gmail.com>
2019-01-31 10:06:01 +0800

    8
    8

---
+++ mall_html/js/ui-main7.js
@@ -0,0 +1,118 @@
+$(function()
+{
+    $(window)
+        .load(function()
+        {
+            var strTimeStamp = parseInt((new Date().getTime())/60000) ; // change per minute
+            $.ajax(
+            {
+                type: "get"
+                ,url:'http://ecapi.pchome.com.tw/cdn/ecshop/adapi/v5/ad&q=onsale&sign=h24%252F3c&d=now&_callback=jsonpcb_onsale_3c&'+ strTimeStamp
+                ,dataType : 'jsonp'
+                ,jsonpCallback : 'jsonpcb_onsale_3c'
+                ,cache : true
+            })
+            .done(function(data)
+            {
+                $.each(data, function(idx, _data)
+                {
+
+                    $.each(_data.Nodes, function(idx, _node)
+                    {
+						
+						$("#BOX_A")
+                            .append('<li id="box'+idx+'" class="box">'+
+                                        '<a href="'+_node.Link.Url+'" target="_blank"><div class="imgs"><img src="'+_node.Img2.Src+'" alt=""></div></a>'+
+                                	 '</li>');	
+							
+					});
+					
+                });
+            });
+			
+			
+			 $.ajax(
+            {
+                type: "get"
+                ,url:'http://ecapi.pchome.com.tw/cdn/ecshop/adapi/v5/ad&q=onsale&sign=h24%252Fce&d=now&_callback=jsonpcb_onsale_ce&'+ strTimeStamp
+                ,dataType : 'jsonp'
+                ,jsonpCallback : 'jsonpcb_onsale_ce'
+                ,cache : true
+            })
+            .done(function(data)
+            {
+                $.each(data, function(idx, _data)
+                {
+
+                    $.each(_data.Nodes, function(idx, _node)
+                    {
+						
+						$("#BOX_B")
+                            .append('<li class="box">'+
+                                        '<a href="'+_node.Link.Url+'" target="_blank"><div class="imgs"><img src="'+_node.Img2.Src+'" alt=""></div></a>'+ 
+                                	 '</li>') ;
+							
+					});
+					
+                });
+            });
+			
+			
+			 $.ajax(
+            {
+                type: "get"
+                ,url:'http://ecapi.pchome.com.tw/cdn/ecshop/adapi/v5/ad&q=onsale&sign=h24%252Ffood&d=now&_callback=jsonpcb_onsale_food&'+ strTimeStamp
+                ,dataType : 'jsonp'
+                ,jsonpCallback : 'jsonpcb_onsale_food'
+                ,cache : true
+            })
+            .done(function(data)
+            {
+                $.each(data, function(idx, _data)
+                {
+
+                    $.each(_data.Nodes, function(idx, _node)
+                    {
+						
+						 $("#BOX_C")
+                            .append('<li class="box">'+
+                                        '<a href="'+_node.Link.Url+'" target="_blank"><div class="imgs"><img src="'+_node.Img2.Src+'" alt=""></div></a>'+
+                                	 '</li>');
+									
+							
+					});
+					
+                });
+            });
+			
+			$.ajax(
+            {
+                type: "get"
+                ,url:'http://ecapi.pchome.com.tw/ecshop/adapi/v5/ad&q=welcome&d=now&_callback=json_welcome&'+ strTimeStamp
+                ,dataType : 'jsonp'
+                ,jsonpCallback : 'json_welcome'
+                ,cache : true
+            })
+            .done(function(data)
+            {
+                $.each(data, function(idx, _data)
+                {
+                    if (!_data.Nodes[0])
+                    {
+                        return ;
+                    }
+
+                    $.each(_data.Nodes, function(idx, _node)
+                    {
+
+                        $("#BOX_D #titlebodya")
+                            .append('<li class="boxa">'+
+									'<a id="txtBox" href="'+_node.Link.Url+'" target="_blank">'+'<div class="imga"><img src="'+_node.Img.Src+'" alt=""></div><div class="txt0">'+_node.Link.Text+'</div><a href="'+_node.Link.Url+'" target="_blank"><div class="txt1">'+_node.Link.Text1+'</div></a><a href="'+_node.Link.Url+'" target="_blank"><div class="txt2"><span class="txt3">$</span><span class="txt4">'+_node.Link.Text2+'</span><span class="txt5">搶></span></div></a>'+'</a>'	
+                                	+'</li>') ;
+
+                    }) ;
+                }) ;
+            });
+			
+        }) ;
+});
