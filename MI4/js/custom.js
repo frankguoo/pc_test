707d37a05f2568fffa94fbbe2be7d5debde7c5ec
PChomeFrank <frank61012@gmail.com>
2019-03-14 11:57:44 +0800

    ff
    ff

---
+++ MI4/js/custom.js
@@ -0,0 +1,6 @@
+
+$(function() {
+if($('.subSlider ul li').length <= 2){
+	$('.subSlider ul li').removeClass('item col-xs-6 col-sm-6 col-md-3').addClass('item col-xs-12 col-sm-12 col-md-6');
+}
+});
