3bae078db883b7824ca5bbc2225dfb830da6a983
PChomeFrank <frank61012@gmail.com>
2019-05-09 16:57:25 +0800

    gsdg
    gsdgwe

--- cake3/js/common_v3.js
+++ cake3/js/common_v3.js
@@ -2,16 +2,41 @@ $(function() {
   $(".ball").draggable();
 });
 
-$(".note").on("click", function() {
+$(".save").on("click", function() {
+  html2canvas(document.getElementById("game")).then(function(canvas) {
+    var myImage = canvas.toDataURL("image/png");
+    saveAs(myImage, 'image.png');
+    });
+});
+
+function saveAs(uri, filename) {
+  var link = document.createElement('a');
+  if (typeof link.download === 'string') {
+    link.href = uri;
+    link.download = filename;
+
+    //Firefox requires the link to be in the body
+    document.body.appendChild(link);
+
+    //simulate click
+    link.click();
+
+    //remove the link when done
+    document.body.removeChild(link);
+  } else {
+    window.open(uri);
+  }
+}
+$(".note2").on("click", function() {
   html2canvas(document.getElementById("game")).then(function(canvas) {
     console.log(canvas.toDataURL("image/png"));
     var myImage = canvas.toDataURL("image/png");
     $(".lightbox").fadeIn(200);
-    $(".image")
-      .attr("src", myImage)
-      .fadeIn(200);
-  });
+    $(".image").attr("src", myImage).fadeIn(200);
+    });
 });
+
+
 
 $(".closebox").on("click", function() {
   $(".lightbox").css("display", "none");
