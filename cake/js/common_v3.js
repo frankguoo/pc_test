61d179c0267fbe3cacffbdac1c5adc7b06421a43
PChomeFrank <frank61012@gmail.com>
2019-05-09 11:33:09 +0800

    rr
    r

--- cake/js/common_v3.js
+++ cake/js/common_v3.js
@@ -1,13 +1,1 @@
-$('.note').on('click', function(){
-    html2canvas($(".game_wrap"), {
-            onrendered: function(canvas) {
-                    var myImage = canvas.toDataURL("image/png");
-                $('.lightbox').fadeIn(200);
-                $('.image').attr('src', myImage).fadeIn(200);
-            }
-    });
-});
 
-$('.closebox').on('click', function(){
-    $('.lightbox').css("display","none");
-});
